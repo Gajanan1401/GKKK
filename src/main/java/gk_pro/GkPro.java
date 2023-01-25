96f257ad188a286eada6c822a3c86dcb61b66038
Gajanan kadam <gajanankadam2000@gmail.com>
2023-01-25 12:36:02 +0530

    test1 for docer

---
+++ src/main/java/gk_pro/GkPro.java
@@ -0,0 +1,25 @@
+package gk_pro;
+
+import java.util.Scanner;
+
+public class GkPro {
+	
+	
+	public static boolean checkNum(int num) {
+		return num%2==0;
+	}
+	
+	public static void main(String[] args) {
+//		  Scanner sc =new Scanner(System.in);
+//		  System.out.println("enetr the num1 and num2");
+//		  int num1=sc.nextInt();
+//		  int num2=sc.nextInt();
+//		  
+//		  int num3=num1+num2;
+//		  System.out.println("the sum of the number is  = "+num3);
+		
+		checkNum(10);
+		  
+	}
+
+}
---
+++ src/test/java/gk_pro/TestMain.java
@@ -0,0 +1,13 @@
+package gk_pro;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+			
+	@Test
+	public  void checkTest() {
+		assertTrue(GkPro.checkNum(13));
+	}
+}
